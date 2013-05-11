d8a9ce3cababe7ab4a9a98889032fc6d5b802702
Mikhail Knuykhov <piroman@myconfig.ru>
2013-05-11 12:59:31 +0400

    fix console

--- src/console.c
+++ src/console.c
@@ -11,42 +11,59 @@
 void console_start(int argc, char* argv[])
 {
     int n,i;
-    char *buff = malloc(sizeof(char) * READ_BUFF_SIZE);
+    char **buff = malloc(sizeof(char*) * READ_BUFF_SIZE);
+    int command_count = 0;
+    buff[command_count] = malloc(sizeof(char) * READ_BUFF_SIZE);
     int sockfd = 0;
     int console_efd = client_epoll_create();
-    int command_count = 0;
+
 
     GHashTable* socket_send_hash = g_hash_table_new(g_str_hash, g_str_equal);
     GHashTable* socket_recaive_hash = g_hash_table_new(g_str_hash, g_str_equal);
     GHashTable* commands_hash = g_hash_table_new(g_str_hash, g_str_equal);
 
-    memset(buff, 0, READ_BUFF_SIZE);
+    memset(buff[command_count], 0, READ_BUFF_SIZE);
 
     char n_str[]="\0\0\0\0\0\0\0\0\0";
-    while ((n = read(STDIN_FILENO, buff, READ_BUFF_SIZE-1)) > 0){
-	if (strlen(buff)==1){
+    while ((n = read(STDIN_FILENO, buff[command_count], READ_BUFF_SIZE-1)) > 0){
+	if (strlen(buff[command_count])==1){
 	    break;
 	}
 	debug("readed!");
-	sprintf(n_str, "%d", command_count++);
+	sprintf(n_str, "%d", command_count);
 	g_hash_table_insert(commands_hash, n_str, buff);
-	buff = malloc(sizeof(char) * READ_BUFF_SIZE);
-	memset(buff, 0, READ_BUFF_SIZE);
+	command_count++;
+	buff[command_count] = malloc(sizeof(char) * READ_BUFF_SIZE);
+	memset(buff[command_count], 0, READ_BUFF_SIZE);
     }
 
     debug("Run Command Batch!");
 
     for (i=0; i<command_count; i++){
+	debug("in cycle");
 	int old_size = g_hash_table_size(socket_send_hash);
-	char* str_i = malloc(sizeof(char)*128);
-	sprintf(str_i, "%d", i);
-	buff = g_hash_table_lookup(commands_hash, str_i);
-	free(str_i);
-	int r = openproto_run_command(buff, console_efd, socket_send_hash, socket_recaive_hash, commands_hash, command_count, i, sockfd);
-	free(buff);
+//	char* str_i = malloc(sizeof(char)*128);
+//	debug("malloc ok");
+//	sprintf(str_i, "%d", i);
+//	debug("sprintf ok");
+//	debug(str_i);
+	//buff = g_hash_table_lookup(commands_hash, str_i);
+	if (!buff[i]){
+	    debug("Command Batch finished");
+	    debug("Exiting");
+	    return;
+	}
+	debug("buff ok");
+//	free(str_i);
+//	debug("free str_i ok");
+	int r = openproto_run_command(buff[i], console_efd, socket_send_hash, socket_recaive_hash, commands_hash, command_count, i, sockfd);
+	debug("command run ok");
+	free(buff[i]);
+	debug("free buff ok");
 	int new_size = g_hash_table_size(socket_send_hash);
 	if (new_size > old_size){
 	    //connection open!
+	    debug("connection opened");
 	    sockfd = r;
 	    break;
 	}
