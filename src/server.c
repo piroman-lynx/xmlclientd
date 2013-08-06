db19a592a4323a394442aca1d38fb5af9a7342d4
piroman <piroman@myconfig.ru>
2013-08-06 17:58:38 +0400

    server test

--- src/server.c
+++ src/server.c
@@ -1,12 +1,57 @@
 #include "debug.h"
 #include "const.h"
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
 
 void set_epoll_on_shared_socket(int socket)
 {
+    debug("Client is run");
+
+    char buf[RECV_BUF*2];
+    struct epoll_event *events;
+    events = calloc (MAXEVENTS, sizeof (struct epoll_event));
+
     while (PERMANENT_CYCLE)
     {
-	debug("Cleint is run!");
-	sleep(25);
+      int n, i;
+      n = epoll_wait (socket, events, MAXEVENTS, -1);
+      for (i = 0; i < n; i++){
+            //115 - operation in progress
+            if ((errno != 115) && ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) || (!(events[i].events & EPOLLIN)))) {
+                perror("Initializator: Epoll");
+                logger("Initializator: epoll error!", DEBUG_ERROR);
+                close (events[i].data.fd);
+                continue;
+            }else{
+              int done = 0;
+              while (1){
+                  ssize_t count;
+                  char buf[RECV_BUF];
+                  memset(buf, 0, RECV_BUF);
+                  count = read (events[i].data.fd, buf, sizeof buf);
+                  if (count == -1){
+                      if (errno != EAGAIN){
+                          perror ("Initializator: read");
+                          done = 1;
+                      }
+                      break;
+                  } else if (count == 0) {
+                      done = 1;
+                      debug("Initializator: done");
+                      break;
+                  } else {
+                      debug_s("Readed: ", buf);
+                  }
+              }
+              if (done){
+                  printf ("Initializator: Closed connection on descriptor %d\n", events[i].data.fd);
+                  close (events[i].data.fd);
+              }
+          }
+      }
     }
 }
 
